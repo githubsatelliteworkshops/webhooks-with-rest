90d66a00d5a501e209275ac6fd6089a3077fdeae
Kevin Klopfenstein <kklopfenstein@github.com>
2023-06-09 14:24:06 -0400

    Fix vendored gems

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -3,8 +3,4 @@ set -e
 
 cp /data/apps/changelogger/.bundle/config /usr/local/bundle/config
 
-# needed to fix issue with nokogiri: `Could not find nokogiri-1.10.9 in any of the sources`
-# this probably isn't the best thing to do from an entrypoint, but it works for now
-bundle install --path vendor/bundle
-
 exec "$@"
